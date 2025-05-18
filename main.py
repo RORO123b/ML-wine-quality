fc72993d2d219c788f096a9faa32a409e3d3a53a
Robert Barbulescu <robert.cristian.barbulescu@gmail.com>
2025-05-19 00:32:32 +0300

    Adaugat detalii grafice + Boxplot

--- main.py
+++ main.py
@@ -13,10 +13,23 @@ print(df.describe(), "\n")
 
 # Histograma
 df.hist(bins=10, figsize=(10, 10))
+plt.suptitle('Histograma caracteristicilor vinului')
+plt.tight_layout()
 plt.show()
 
 # Countplot
 plt.figure(figsize=(10, 6))
 sns.countplot(x='quality', data=df, palette='coolwarm', hue='quality')
+plt.title('Numarul de vinuri in functie de calitate')
+plt.xlabel('Calitate')
+plt.ylabel('Numar de vinuri')
+plt.legend(title='Calitate')
+plt.show()
 
+# Boxplot
+plt.figure(figsize=(12, 6))
+sns.scatterplot(x='quality', y='alcohol', data=df, palette='coolwarm', hue='quality')
+plt.title('Boxplot al continutului de alcool in functie de calitate')
+plt.xlabel('Calitate')
+plt.ylabel('Alcool')
 plt.show()
