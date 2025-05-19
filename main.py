3a2dc56485c9ca2c8681c5f287daf2d0edc25ad7
Robert Barbulescu <robert.cristian.barbulescu@gmail.com>
2025-05-19 22:48:37 +0300

    Final commit

--- main.py
+++ main.py
@@ -49,9 +49,11 @@ def antrenarea_modelului(df):
     # Impartirea setului de date in seturi de antrenare si testare
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
 
-    # Antrenarea modelului
-    model = RandomForestRegressor(n_estimators=250, random_state=42)
-    model.fit(X_train, y_train)
+    # Antrenarea modelului --> DONE
+    ### model = RandomForestRegressor(n_estimators=250, random_state=42)
+    ### model.fit(X_train, y_train)
+
+    model = joblib.load("model_forest.pkl")
 
     # Predictia
     y_pred = model.predict(X_test)
@@ -67,10 +69,12 @@ def antrenarea_modelului(df):
     train_set.to_csv('train_set.csv', index=False)
     test_set.to_csv('test_set.csv', index=False)
 
-    # Salvarea modelului
-    joblib.dump(model, "model_forest.pkl")
+    # Salvarea modelului --> DONE
+    ### joblib.dump(model, "model_forest.pkl")
 
 df = pd.read_csv('winequalityN.csv')
+
+analiza_datelor(df)
 
 for col in ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'pH', 'sulphates']:
     df[col] = df[col].fillna(df[col].mean())
@@ -79,6 +83,5 @@ encoder = LabelEncoder()
 df['type'] = encoder.fit_transform(df['type'])
 scaler = MinMaxScaler()
 df[df.columns] = scaler.fit_transform(df[df.columns])
-print(df.head())
 
-# antrenarea_modelului(df) --> DONE
+antrenarea_modelului(df)
